233248a79b5c54088405db66e314b70043b968cb
Stephane Franjou <sfr@sierrawireless.com>
2016-11-08 10:25:49 +0100

    Correct typo in ring indicator APIs description
    Resolves: LE9XXX-333
    Change-Id: I756e02269c5865bbe5f544ebb12d21424f175036

--- components/le_pa/pa_riPin.c
+++ components/le_pa/pa_riPin.c
@@ -38,7 +38,7 @@ le_result_t pa_riPin_Init
  * @return
  *      - LE_OK           The function succeeded.
  *      - LE_FAULT        The function failed.
- *      - LE_UNSUPPORTED  The platform does not support this operation failed.
+ *      - LE_UNSUPPORTED  The platform does not support this operation.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t pa_riPin_AmIOwnerOfRingSignal
@@ -57,7 +57,7 @@ le_result_t pa_riPin_AmIOwnerOfRingSignal
  * @return
  *      - LE_OK           The function succeeded.
  *      - LE_FAULT        The function failed.
- *      - LE_UNSUPPORTED  The platform does not support this operation failed.
+ *      - LE_UNSUPPORTED  The platform does not support this operation.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t pa_riPin_TakeRingSignal
@@ -75,7 +75,7 @@ le_result_t pa_riPin_TakeRingSignal
  * @return
  *      - LE_OK           The function succeeded.
  *      - LE_FAULT        The function failed.
- *      - LE_UNSUPPORTED  The platform does not support this operation failed.
+ *      - LE_UNSUPPORTED  The platform does not support this operation.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t pa_riPin_ReleaseRingSignal
