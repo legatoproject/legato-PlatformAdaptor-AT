bcf6867fe5d229a08a537e6d4bf30c76d1ecb563
Patrick Lai <plai@sierrawireless.com>
2018-07-09 12:44:31 +0800

    [MDC] Add extended functions to get disconnect reason for IPv4v6 session
    In modem data connection IPV4V6 mode, these extended functions can
    specify which session's disconnect reason to get.
    
    Resolves: LE-10736
    Cherry-pick: LEAR7M-372
    Change-Id: I2215f293a262e418f678e4f17fb516995dfed9d6
    Signed-off-by: Patrick Lai <plai@sierrawireless.com>

--- components/le_pa/pa_mdc.c
+++ components/le_pa/pa_mdc.c
@@ -1412,3 +1412,19 @@ le_result_t pa_mdc_MapProfileOnNetworkInterface
 {
     return LE_UNSUPPORTED;
 }
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Get the connection failure reason for IPv4v6 mode
+ *
+ */
+//--------------------------------------------------------------------------------------------------
+void pa_mdc_GetConnectionFailureReasonExt
+(
+    uint32_t profileIndex,                            ///< [IN] The profile to use
+    le_mdc_Pdp_t pdp,                                 ///< [IN] The failure reason pdp type
+    pa_mdc_ConnectionFailureCode_t** failureCodesPtr  ///< [OUT] The specific Failure Reason codes
+)
+{
+    *failureCodesPtr = NULL;
+}
