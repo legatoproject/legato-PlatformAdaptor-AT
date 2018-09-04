a9369b4c639c819f138a132d6b7c90f6aa711eb9
Oussama ABID <oabid@sierrawireless.com>
2018-09-04 15:04:55 +0200

    [GNSS] Retrieve leap seconds information
    Resolves: LE-9612
    Relates: LEAR7M-241
    Change-Id: Ia25f0400a6cf5ac3d6eadaff74f42ecd261fe440

--- components/le_pa_gnss/gnss_at.c
+++ components/le_pa_gnss/gnss_at.c
@@ -479,6 +479,31 @@ le_result_t pa_gnss_GetNmeaSentences
 
 //--------------------------------------------------------------------------------------------------
 /**
+ * This function gets leap seconds information
+ *
+ * @return
+ *  - LE_OK           The function successed
+ *  - LE_FAULT        The function failed
+ *  - LE_TIMEOUT      The function timeout
+ *  - LE_UNSUPPORTED  Not supported on this platform
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t pa_gnss_GetLeapSeconds
+(
+    uint64_t* gpsTimePtr,              ///< [OUT] The number of milliseconds of GPS time since
+                                       ///<       Jan. 6, 1980
+    int32_t* currentLeapSecondsPtr,    ///< [OUT] Current UTC leap seconds value in milliseconds
+    uint64_t* changeEventTimePtr,      ///< [OUT] The number of milliseconds since Jan. 6, 1980
+                                       ///<       to the next leap seconds change event
+    int32_t* nextLeapSecondsPtr        ///< [OUT] UTC leap seconds value to be applied at the
+                                       ///<       change event time in milliseconds
+)
+{
+    return LE_FAULT;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
  * Component initializer called automatically by the Legato application framework.
  * This is not used because we want to make sure that GNSS is available before initializing
  * the platform adapter.
