2ae1e383b1ea67f972d59799e9fa25660b99088e
Kevin Cao <KCao@sierrawireless.com>
2021-01-29 09:59:42 -0800

    Sync AVC package downloader between ulegato and Master branch
    On ulegato branch there are lots of changes are made to fix EURY
    tickets especially related to FOTA. These changes need to be
    cherry-picked to Master branch.
    
    Resolves: LE-15755
    Change-Id: Ia0c7a57d30026f6953de9b93ca35167cfd1e613e

--- components/le_pa/pa_sim.c
+++ components/le_pa/pa_sim.c
@@ -168,6 +168,24 @@ le_result_t pa_sim_SelectCard
 
 //--------------------------------------------------------------------------------------------------
 /**
+ * This function gets sim selection mode.
+ *
+ * @return LE_FAULT         The function failed.
+ * @return LE_TIMEOUT       No response was received.
+ * @return LE_OK            The function succeeded.
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t __attribute__((weak)) pa_sim_GetSimMode
+(
+    le_sim_SimMode_t*  simModePtr     ///< [OUT] The SIM selection mode
+)
+{
+    *simModePtr = LE_SIM_FORCE_EXTERNAL;
+    return LE_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
  * This function get the card on which operations are operated.
  *
  * @return LE_FAULT         The function failed.
